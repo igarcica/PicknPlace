236fbe81b3a6b67bfedf49e7cb23c41b6c8c459e
Irene Garcia-Camacho <igarcia@iri.upc.edu>
2023-02-03 12:08:09 +0000

    Experiments branch - Canonical state def metric + Save metrics CSV
    /vision_pp:
    
    - Added the paramters to compute the deformation metric (depth mean at each grid) according to the canonical state of each phase (min depth, xy grid threshold, PC grid sizes)
    
    /data:
    
    - execution.py
    	- Publishes the content in PCD file of the current folder 8seg as PC topic.
    	- Writes in a csv file the file names (tags) of the PCD files used.

--- data/save_metric/execution.py
+++ data/save_metric/execution.py
@@ -1,20 +1,27 @@
 import os
 import subprocess
- 
+import csv
+
+## Save trial tags intro a csv
+data_filename = "./o1_gr_z_tags.csv"
+my_file = open(data_filename, "wb")
+wr = csv.writer(my_file, quoting=csv.QUOTE_NONNUMERIC)
+
+## Publish pcd files as pointcloud topic during 8seg 
+print("Reading PCD files") 
 directory = './'
 for filename in sorted(os.listdir(directory)):
     f = os.path.join(directory, filename)
     if os.path.isfile(f) and filename.endswith('.pcd'):
-        command = "timeout 10s rosrun pcl_ros pcd_to_pointcloud " + filename +" 0.1"
-        os.system(command)
+        print(filename)
+        command = "timeout 8s rosrun pcl_ros pcd_to_pointcloud " + filename +" 0.1"
         print(command)
-#        hola = subprocess.check_output(["rosrun", "pcl_ros", "pcd_to_pointcloud", filename, "0.1"], shell=True, timeout=500000000)
+        os.system(command)
+        wr.writerow((filename,''))
 
 
-## TO DO
-# Ejecutar archivos en orden - OK
-# Cntrl C automatico - OK
-# Timeout - OK
+
+
 
 
 ## REFERENCES
